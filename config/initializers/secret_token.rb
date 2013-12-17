cf902809ce590140f69b96bdfd33b06374a5778b
Krystle Bates <krebates@gmail.com>
2013-12-17 18:36:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birds3::Application.config.secret_key_base = '55344248804815e80fb8470af296b0c4debbaed03aede202f80f542d3c45d9d8e90115d61a14f022a793f95cf89a90678f8c552a41496bdc4da46a5a4d8b7bc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Birds3::Application.config.session_store :cookie_store, key: '_birds_3_session'
